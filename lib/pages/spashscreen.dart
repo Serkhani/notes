b10d9b8122253039079c7ee45e0e144210a85cda
Serkhani <deviper03@gmail.com>
2022-10-11 16:24:21 +0100

    adding splash screen

---
+++ lib/pages/spashscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 50.0,
+          width: 50.0, 
+          child: CircularProgressIndicator()
+        )
+      )
+    );
+  }
+}
