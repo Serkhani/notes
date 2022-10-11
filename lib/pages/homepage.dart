37fe4f40185765575bb50b6e632fd87ab47ef644
Serkhani <deviper03@gmail.com>
2022-10-11 15:12:06 +0100

    homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
-
+import '../models/note_tile.dart';
+import '../utils/note.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
-        shrinkWrap: true,
         slivers: [
-          SliverAppBar(
-            title: Text("Notes"),
-          )
+          const SliverAppBar(
+            elevation: 0.0,
+            pinned: true,
+            centerTitle: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Text("x notes"),
+              title: Text("N O T E S"),
+              centerTitle: false,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: StreamBuilder(
+              stream: Stream.periodic(
+                const Duration(seconds: 1), (computationCount) {
+                  return Note(
+                      title: "title",
+                      imageUrl: "imageUrl",
+                      content: "content");
+                },
+              ).take(8),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    // itemCount: ,
+                    itemBuilder: (context, index) {
+                      return NoteTile(note: snapshot.data!);
+                    },
+                  );
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
