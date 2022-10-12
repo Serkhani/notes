88e92ae7b4842d31474c3bea6d8a0012b13205b0
Serkhani <deviper03@gmail.com>
2022-10-12 16:15:45 +0100

    updating routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
+import '../pages/authpage.dart';
 import '../pages/notespage.dart';
 import '../pages/homepage.dart';
+import '../pages/onboarding_screen.dart';
 import '../pages/splashscreen.dart';
 
 class AppRoutes {
   static List<GetPage> routes() => [
         GetPage(name: "/", page: () => const SplashScreen()),
-        // GetPage(name: "/onboarding", page: () => SplashScreen())
+        GetPage(name: "/onboading", page: () => const OnboardingScreen()),
+        GetPage(name: "/auth", page: () => const AuthPage()),
         GetPage(name: "/home", page: () => const MyHomePage()),
         GetPage(name: "/notes", page: () => const NotesPage())
       ];
