687ecd6bc8638f7e28576e7e71cf61a43fa481a3
Serkhani <deviper03@gmail.com>
2022-10-18 15:25:26 +0100

    for onboarding screen object data

---
+++ lib/utils/onboarding_elements.dart
@@ -0,0 +1,9 @@
+class OnBoardingElement {
+  final String imgPath;
+  final String notesTitle;
+  final String notesSubtitle;
+  OnBoardingElement(
+      {required this.imgPath,
+      required this.notesTitle,
+      required this.notesSubtitle});
+}
